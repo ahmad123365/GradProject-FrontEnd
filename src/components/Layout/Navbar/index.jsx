7eb1018daaaa96bfca3c903a3c8cdff51fe8ebbb
Ahmad Hanafi <83533499+ahmad123365@users.noreply.github.com>
2023-12-08 15:53:52 +0200

    finished most of the navbar

--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import {ShoppingCartOutlined} from "@ant-design/icons"
 
-const index = () => {
+const Navbar = () => {
   return (
-	<div className='w-full h-15 bg-[#00A9FF] flex'>
+	<div className='w-full min-h-[2.5rem] bg-[#00A9FF] flex justify-around shadow-xl'>
 		  <img
-		  	src={process.env.PUBLIC_URL + "/logo.png"}
+		  	src={process.env.PUBLIC_URL + "/images/logo-horizental.png"}
 			alt="logo"
-			className='w-[4rem] p-2 select-none pointer-events-none'
+			className='w-[7rem] px-2 py-3 select-none pointer-events-none'
 		  />
+		  <ul className='flex justify-between gap-5 items-center mx-auto'>
+			  <NavLink className='w-[4rem] h-full leading-[65px] text-center select-none text-xl text-white hover:leading-[55px] hover:border-b-4 transition-all duration-300 '
+			style={({ isActive }) => (isActive ? { color: "#9FF68B", borderBottomWidth: "4px", borderColor: "#9FF68B"} : {})}
+			to="/">Home</NavLink>
+			  <NavLink className='w-[4rem] h-full leading-[65px] text-center select-none text-xl text-white hover:leading-[55px] hover:border-b-4 transition-all duration-300 '
+			style={({ isActive }) => (isActive ? { color: "#9FF68B", borderBottomWidth: "4px", borderColor: "#9FF68B"} : {})}
+			to="/chat">Chat</NavLink>
+			  <NavLink className='w-[4rem] h-full leading-[65px] text-center select-none text-xl text-white hover:leading-[55px] hover:border-b-4 transition-all duration-300 '
+			style={({ isActive }) => (isActive ? { color: "#9FF68B", borderBottomWidth: "4px", borderColor: "#9FF68B"} : {})}
+			to="/about">About</NavLink>
+			  
+		  </ul>
+		  <div>
+		  <ShoppingCartOutlined />
+		  <ShoppingCartOutlined />
+		  <ShoppingCartOutlined />
+		  </div>
+
 	</div>
   )
 }
 
-export default index
+export default Navbar

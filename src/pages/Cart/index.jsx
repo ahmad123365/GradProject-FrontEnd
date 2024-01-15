8bc6fd4f7643e2fa25023fff61e0085ad559526a
Ahmad Hanafi <83533499+ahmad123365@users.noreply.github.com>
2024-01-15 14:45:18 +0200

    fixed cart

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 
 const Chat = () => {
@@ -9,7 +10,7 @@ const Chat = () => {
         <div className="w-3/4 bg-white px-10 py-10">
           <div className="flex justify-between border-b pb-8">
             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-            <h2 className="font-semibold text-2xl">3 Items</h2>
+            {/* <h2 className="font-semibold text-2xl">Items</h2> */}
           </div>
           <div className="flex mt-10 mb-5">
             <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
@@ -20,11 +21,11 @@ const Chat = () => {
           <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
             <div className="flex w-2/5">
               <div className="w-20">
-                <img className="h-24" src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z" alt="" />
+                <img className="h-24" src="" alt="" />
               </div>
               <div className="flex flex-col justify-between ml-4 flex-grow">
-                <span className="font-bold text-sm">Iphone 6S</span>
-                <span className="text-red-500 text-xs">Apple</span>
+                <span className="font-bold text-sm">Book 1</span>
+                <span className="text-red-500 text-xs">Author</span>
                 <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
               </div>
             </div>
@@ -48,8 +49,8 @@ const Chat = () => {
                 <img className="h-24" src="https://drive.google.com/uc?id=10ht6a9IR3K2i1j0rHofp9-Oubl1Chraw" alt="" />
               </div>
               <div className="flex flex-col justify-between ml-4 flex-grow">
-                <span className="font-bold text-sm">Xiaomi Mi 20000mAh</span>
-                <span className="text-red-500 text-xs">Xiaomi</span>
+                <span className="font-bold text-sm">Book 2</span>
+                <span className="text-red-500 text-xs">Author</span>
                 <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
               </div>
             </div>
@@ -73,8 +74,8 @@ const Chat = () => {
                 <img className="h-24" src="https://drive.google.com/uc?id=1vXhvO9HoljNolvAXLwtw_qX3WNZ0m75v" alt="" />
               </div>
               <div className="flex flex-col justify-between ml-4 flex-grow">
-                <span className="font-bold text-sm">Airpods</span>
-                <span className="text-red-500 text-xs">Apple</span>
+                <span className="font-bold text-sm">Book 3</span>
+                <span className="text-gray-500 text-xs">Author</span>
                 <a href="#" className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
               </div>
             </div>
@@ -90,12 +91,12 @@ const Chat = () => {
             <span className="text-center w-1/5 font-semibold text-sm">$150.00</span>
             <span className="text-center w-1/5 font-semibold text-sm">$150.00</span>
           </div>
-  
-          <a href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
-        
-            <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
+      <NavLink to="/home">
+          <div href="#" className="flex font-semibold text-[#00A9FF] text-sm mt-10">
+            <svg className="fill-current mr-2 text-[#00A9FF] w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
             Continue Shopping
-          </a>
+          </div>
+      </NavLink>
         </div>
   
         <div id="summary" className="w-1/4 px-8 py-10">
@@ -110,17 +111,14 @@ const Chat = () => {
               <option>Standard shipping - $10.00</option>
             </select>
           </div>
-          <div className="py-10">
-            <label for="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Promo Code</label>
-            <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full" />
-          </div>
-          <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Apply</button>
+         
+          <button className="w-[90%] bg-[#00A9FF] text-[#fff] mt-3 py-1 px-2 rounded-xl text-[.8rem] font-semibold hover:text-[#00A9FF] hover:bg-[#eee] transition-colors duration-300">Apply</button>
           <div className="border-t mt-8">
             <div className="flex font-semibold justify-between py-6 text-sm uppercase">
               <span>Total cost</span>
               <span>$600</span>
             </div>
-            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Checkout</button>
+            <button className="w-[90%] bg-[#00A9FF] text-[#fff] mt-3 py-1 px-2 rounded-xl text-[.8rem] font-semibold hover:text-[#00A9FF] hover:bg-[#eee] transition-colors duration-300">Checkout</button>
           </div>
         </div>
   
